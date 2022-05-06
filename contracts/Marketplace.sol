1846d953214ae4908478bffb9099648556f12bd3
AlexMuia31 <alexmuia31@gmail.com>
2022-05-06 18:56:45 +0300

    make Item

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -9,8 +9,40 @@ contract Marketplace is ReentrancyGuard {
     uint256 public immutable feePercent; // the fee percentage on sales
     uint256 public itemCount;
 
+    struct Item {
+        uint256 itemId;
+        IERC721 ntf;
+        uint256 tokenId;
+        uint256 price;
+        address payable seller;
+        bool sold;
+    }
+
+    mapping(uint256 => Item) public items; // storing the items
+
     constructor(uint256 _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    function makeItem(
+        IERC721 _nft,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero");
+        //increment itemCount
+        itemCount++;
+        // transfer nft
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        // add new item to items mapping
+        items[itemCount] = item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
     }
 }
