284b94b7394d697fb1ed87ac5a6534c68e35e818
AlexMuia31 <alexmuia31@gmail.com>
2022-07-13 14:45:33 +0300

    refactoring code

--- src/App.js
+++ src/App.js
@@ -8,54 +8,16 @@ import Explore from "./components/Explore";
 import MyPurchases from "./components/MyPurchases";
 import { ethers } from "ethers";
 import { useState } from "react";
-import MarketplaceAbi from "./Marketplace.json";
-import NFTAbi from "./NFT.json";
-
-const MarketplaceAddress = "0xa005fDf3A53AddcB80b041B0A3a018d8B381aC1a";
-const NFTAddress = "0xD5896f1F9DC8B98e84c30a2aC559d63Eba4dD1bd";
 
 function App() {
-  const [account, setAccount] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [nft, setNFT] = useState({});
-  const [marketplace, setMarketplace] = useState({});
-  //MetaMask login/Connect
-  const web3Handler = async () => {
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    }); //getting accounts from metamask
-    setAccount(accounts[0]);
-    //Get provider from Metamask
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    //Set signer
-    const signer = provider.getSigner();
-    loadContracts(signer);
-  };
-  const loadContracts = async (signer) => {
-    //Get deployed copies of contracts
-    const marketplace = new ethers.Contract(
-      MarketplaceAddress,
-      MarketplaceAbi.abi,
-      signer
-    );
-    setMarketplace(marketplace);
-    const nft = new ethers.Contract(NFTAddress, NFTAbi.abi, signer);
-    setNFT(nft);
-    //set loading to false when the contract loads
-    setLoading(false);
-  };
-
   return (
     <Container maxWidth="xxl" disableGutters={true}>
-      <NavBar account={account} web3Handler={web3Handler} />
+      <NavBar />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
-        <Route
-          path="/explore"
-          element={<Explore marketplace={marketplace} nft={nft} />}
-        />
+        <Route path="/explore" element={<Explore />} />
         <Route path="/myPurchases" element={<MyPurchases />} />
       </Routes>
     </Container>
--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -15,97 +15,49 @@ import { ethers } from "ethers";
 
 import Loader from "./Loader";
 
-const Explore = ({ marketplace, nft }) => {
-  const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const loadMarketplaceItems = async () => {
-    const itemCount = await marketplace.itemCount();
-    let items = [];
-    for (let i = 1; i <= itemCount; i++) {
-      const item = await marketplace.items(i);
-      if (!items.sold) {
-        //get url from nft contract
-        const uri = await nft.tokenURI(item.tokenId);
-        //use uri to fetch the nft metadata stored on ipfs
-        const response = await fetch(uri);
-        const metadata = await response.json();
-        //get total price of item (item price + fee)
-        const totalPrice = await marketplace.getTotalPrice(item.itemId);
-        //Add item to items array
-        items.push({
-          totalPrice,
-          itemId: item.itemId,
-          seller: item.seller,
-          name: metadata.name,
-          description: metadata.description,
-          image: metadata.image,
-        });
-      }
-    }
-    setItems(items);
-    setLoading(false);
-  };
-  const buyMarketItem = async (item) => {
-    await (
-      await marketplace.purchaseItem(item.itemId, { value: item.totalPrice })
-    ).await();
-    loadMarketplaceItems();
-  };
-  useEffect(() => {
-    loadMarketplaceItems();
-  });
-
-  if (loading) return <Loader/>
-
+const Explore = () => {
   return (
     <Box sx={{ background: "#b4b7bf", minHeight: "100vh" }}>
       <Toolbar />
       <Toolbar />
 
       <Box sx={{ display: "flex", justifyContent: "center", margin: "2%" }}>
-        {items.length > 0 ? (
-          <Grid container spacing={2}>
-            {items.map((item, idx) => (
-              <Grid item key={idx} xs={12} sm={6} md={3}>
-                <Card>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="200"
-                      image={item.image}
-                      alt="green iguana"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {item.name}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {item.description}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button
-                      size="small"
-                      sx={{
-                        background: "#000002",
-                        color: "white",
-                        "&:hover, &:focus": { background: "#000002" },
-                      }}
-                      onClick={() => buyMarketItem(item)}
-                    >
-                      Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+        <Grid container spacing={2}>
+          <Grid xs={12} sm={6} md={3}>
+            <Card>
+              <CardActionArea>
+                <CardMedia component="img" height="200" alt="green iguana" />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                  ></Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                  ></Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  size="small"
+                  sx={{
+                    background: "#000002",
+                    color: "white",
+                    "&:hover, &:focus": { background: "#000002" },
+                  }}
+                >
+                  Buy for
+                </Button>
+              </CardActions>
+            </Card>
           </Grid>
-        ) : (
-          <Box>
-            <Typography>No Listed Items</Typography>
-          </Box>
-        )}
+        </Grid>
+
+        <Box>
+          <Typography>No Listed Items</Typography>
+        </Box>
       </Box>
     </Box>
   );
